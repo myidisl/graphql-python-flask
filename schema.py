a2e9d75beb8193ab90e728e069bd09529dd69f5a
ramapra <rama.nurdian89@mgail.com>
2025-05-29 12:22:15 +0700

    Updated requirements

--- schema.py
+++ schema.py
@@ -1,38 +1,43 @@
 import graphene
 
+# Tipe data User
 class User(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
     email = graphene.String()
 
+# Simulasi database sederhana
 mock_users = [
-        {"id": "1", "name": "Budi", "email": "budi@mail.com"},
-        {"id": "2", "name": "Ani", "email": "ani@mail.com"},
-        ]
+    {"id": "1", "name": "Budi", "email": "budi@mail.com"},
+    {"id": "2", "name": "Ani", "email": "ani@mail.com"},
+]
 
+# Query utama
 class Query(graphene.ObjectType):
     user = graphene.Field(User, id=graphene.ID(required=True))
     users = graphene.List(User)
 
     def resolve_user(root, info, id):
-        return next((u for u in mock_users if u["id"] == "id"), None)
-    
+        return next((user for user in mock_users if user["id"] == id), None)
+
     def resolve_users(root, info):
         return mock_users
 
+# Mutation untuk menambahkan user
 class CreateUser(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
         email = graphene.String(required=True)
 
-    user = graphene.Field(User)
+    user = graphene.Field(lambda: User)
 
     def mutate(self, info, name, email):
-        new_user = {"id":str(len(mock_users)+1),"name":name,"email":email}
-        mock_users = append(new_user)
+        new_user = {"id": str(len(mock_users) + 1), "name": name, "email": email}
+        mock_users.append(new_user)
         return CreateUser(user=new_user)
-    
+
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
 
+# Schema lengkap
 schema = graphene.Schema(query=Query, mutation=Mutation)
