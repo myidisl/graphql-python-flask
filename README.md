4282c8365d6c9df36fd5b452b078a0a58a2a23bd
ramapra <rama.nurdian89@mgail.com>
2025-05-29 11:56:14 +0700

    Initial commit: GraphQL with Python Flask

---
+++ README.md
@@ -0,0 +1,12 @@
+# GraphQL dengan Python (Flask + Graphene)
+
+Proyek sederhana untuk belajar GraphQL menggunakan Python dan Flask.
+
+## 🔧 Setup
+```bash
+git clone https://github.com/<username>/graphql-python-flask.git
+cd graphql-python-flask
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python app.py
---
+++ schema.py
@@ -0,0 +1,38 @@
+import graphene
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    email = graphene.String()
+
+mock_users = [
+        {"id": "1", "name": "Budi", "email": "budi@mail.com"},
+        {"id": "2", "name": "Ani", "email": "ani@mail.com"},
+        ]
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(User, id=graphene.ID(required=True))
+    users = graphene.List(User)
+
+    def resolve_user(root, info, id):
+        return next((u for u in mock_users if u["id"] == "id"), None)
+    
+    def resolve_users(root, info):
+        return mock_users
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    user = graphene.Field(User)
+
+    def mutate(self, info, name, email):
+        new_user = {"id":str(len(mock_users)+1),"name":name,"email":email}
+        mock_users = append(new_user)
+        return CreateUser(user=new_user)
+    
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
